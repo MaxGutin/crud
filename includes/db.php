bde725fff009736d613947601c43168b36b025f3
Max Goodman <wargix32bita@gmail.com>
2020-08-26 19:10:03 +1100

    Little editing of $dsn

--- includes/db.php
+++ includes/db.php
@@ -1,27 +1,10 @@
 <?php
-$dsn = 'mysql:dbname=crud;host=localhost';
+$dsn = 'mysql:host=localhost;dbname=crud';
 $db_user = 'root';
 $db_password = 'root';
 try {                                                                           // подключение перехвата исключений
     $pdo = new PDO($dsn, $db_user, $db_password);                               // инициализация объекта PDO и вставка данных для подключения
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // настройка обработки ошибок
-    function db_select($sql, $var) {
-        try {
-            $stmt = $pdo->prepare(SQL_LOGIN);                         // prepare — Подготавливает SQL-запрос к выполнению
-            $stmt->bindParam(':login', $form_data['login']); // bindParam — Привязывает параметр SQL-запроса к POST-переменной
-            $result = $stmt->execute();                                       // execute — Запускает подготовленный запрос на выполнение, сохраняем результат
-            $user_count = $stmt->rowCount();                                  // rowCount() - возвращает количество строк, нужно для проверки.
-        } catch (PDOException $e) {                                           // выводим ошибки PDO (работы с БД)
-            echo '====CATCH=====: ' . $e->getMessage();
-        }
-        if ($user_count > 0) {                                                // найдена ли строка с искомым login в БД
-            $user = $stmt->fetch(PDO::FETCH_ASSOC);                  // rowCount() - возвращает массив данных.
-            if (password_verify($form_data['password'], $user['password'])) { // если пароли сопадают то выполняем авторизацию
-                $_SESSION['user'] = $user;                                    // создаём сессию с именем 'logged_user' и сохраняем там данные пользователя
-                header('Location: user.php?user_id=' . $user['id']);    // перенаправляем на страницу пользователя
-            } else echo "Не правильный пароль.";                              // если не совпадает пароль то вывести сообщение
-        } else echo "Не правильный логин.";                                   // если пользователь не найден то выводим сообщение
-    }
 } catch (PDOException $e) {                                                     // вывод исключений
     print 'Error connection: ' . $e->getMessage();                              // вывод ошибок подключения
 }
