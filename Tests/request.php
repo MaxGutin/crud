579582182c9dbc96be99fa777f7949023d9c8bf2
Max Goodman <wargix32bita@gmail.com>
2020-09-06 22:58:36 +1100

    no matterю

--- Tests/request.php
+++ Tests/request.php
@@ -1,9 +1,9 @@
 <?php
-//require_once '../includes/db.php';
-//try {
-//    $stmt = $pdo->prepare(SQL_CREATE_USERS_TABLE); // prepare — Подготавливает SQL-запрос к выполнению
+require_once '../includes/db.php';
+try {
+//    $stmt = $pdo->prepare(SQL_CREATE_TASKS_TABLE); // prepare — Подготавливает SQL-запрос к выполнению
 //    $result = $stmt->execute(); // execute — Запускает подготовленный запрос на выполнение, сохраняем результат
-//} catch (PDOException $e) {
-//    $e->getMessage();
-//}
+} catch (PDOException $e) {
+    $e->getMessage();
+}
 phpinfo();
